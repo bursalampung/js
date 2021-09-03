9fad0f1be1eda580fa4169350680060013754ea7
bursalampung <bursalampung@gmail.com>
2021-09-03 17:27:38 +0700

    Update and rename lampung2.js to lampung3.js

--- lampung2.js
+++
@@ -1,7 +0,0 @@
-// Add a schema tag to your
-   $.getJSON( "https://cdn.jsdelivr.net/gh/bursalampung/js/lpg7.json", function( data ) {
-     $( "<script/>", {
-       "type": "application/ld+json",
-       "html": JSON.stringify(data)
-     }).appendTo( "head" );
-   });
---
+++ lampung3.js
@@ -0,0 +1,1 @@
+$.getJSON("https://cdn.jsdelivr.net/gh/bursalampung/js/lpg9.jsonld",function(data){$("<script/>",{"type":"application/ld+json",html:JSON.stringify(data)}).appendTo("head");});
